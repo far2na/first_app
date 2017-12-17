9b19b39fcfac8fb1d8e2cc2239300fbe548555b2
far2na <enot.fx@gmail.com>
2017-12-17 17:46:29 +0300

    Helpers

--- app/controllers/greeter_controller.rb
+++ app/controllers/greeter_controller.rb
@@ -1,5 +1,8 @@
 class GreeterController < ApplicationController
   def hello
+    name_array = ["Mary", "Alex", "Bob", "Sam"]
+    @name = name_array.sample
+    @time = Time.now
   end
   def goodbye
 
