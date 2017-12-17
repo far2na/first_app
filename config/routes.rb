880166d5da1432880c77f552d1f0ed0b177a2bea
far2na <enot.fx@gmail.com>
2017-12-17 16:58:03 +0300

    Controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'greeter/hello', to: 'greeter#hello'
+  get 'greeter/goodbuy', to: 'greeter#goodbuy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
